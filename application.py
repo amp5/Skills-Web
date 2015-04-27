92d63752de63dd4ec28292f783eecf8466bf92d9
amp5 <alexandraplassaras@Alexandras-MacBook-Pro.local>
2015-04-27 09:35:17 -0700

    Added requirements doc and currently debugging in virtual env.

--- application.py
+++ application.py
@@ -7,23 +7,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/'):
+@app.route('/')
 def start_page():
 	return render_template("index.html")
 
 
-@app.route('/application-form'):
+@app.route('/application-form')
 def application():
 	return render_template("application-form.html")
 
-@app.route('/application'):
+@app.route('/application')
 def submitted():
 	firstname = request.args.get("firstname")
-    lastname = request.args.get("lastname")
-    salary = request.args.get("salary")
-    job = request.args.get("job")
+	lastname = request.args.get("lastname")
+	salary = request.args.get("salary")
+	job = request.args.get("job")
 
-	return render_template("submitted.html" firstname='firstname', lastname='lastname', salary="salary", job="job")
+	return render_template("submitted.html", firstname=firstname, lastname=lastname, salary=salary, job=job)
 
 
 
